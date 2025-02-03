dc284bbc5575a4266f0e2984b3892101367ae36a
Sathaporn Bunkoed <sathaporn.chok42@gmail.com>
2025-02-03 23:07:08 +0700

    api user => ทดสอบ login / authen / get / getbyid / insert / update / delete postman => export file postmna-pj-msg |  ตอนเพิ่มไฟล์ลงใน postman โดยการ import หรือ drop file

--- api-pj-msg/api/api-user.js
+++ api-pj-msg/api/api-user.js
@@ -10,7 +10,7 @@ router.post("/login", async (req, res) => {
   try {
     const json = req.body;
 
-    if (!json["user_Name"] || !json["user_Password"]) {
+    if (!json["user_name"] || !json["user_pass"]) {
       res.send({
         status: "400",
         message: "WARNING",
@@ -19,14 +19,16 @@ router.post("/login", async (req, res) => {
       return;
     }
 
-    const [res_user] = await db.query(
-      `
-            SELECT user_Code,user_Name,role_Id,user_Status FROM users 
-            WHERE user_Name = ? 
-            AND user_Password = ? 
-            AND user_Status = ?
-            AND user_IsUse = ?`,
-      [json["user_Name"], md5(json["user_Password"]).toUpperCase(), "1", "1"]
+    const [res_user] = await db.query(`
+            SELECT 
+            user_id
+            ,name AS user_name
+            ,role AS user_role
+            ,phone AS user_phone
+            FROM user 
+            WHERE name = ? 
+            AND password = ?`,
+      [json["user_name"], md5(json["user_pass"])]
     );
 
     if (!res_user || !res_user[0]) {
@@ -49,113 +51,107 @@ router.post("/login", async (req, res) => {
   }
 });
 
-// //authen
-// router.post("/authen", async (req, res) => {
-//   try {
-//     const json = req.body;
+//authen
+router.post("/authen", async (req, res) => {
+  try {
+    const json = req.body;
 
-//     if (!json) {
-//       res.send({
-//         status: "400",
-//         message: "WARNING",
-//         detail: "No Data found",
-//       });
-//       return;
-//     }
+    if (!json) {
+      res.send({
+        status: "400",
+        message: "WARNING",
+        detail: "No Data found",
+      });
+      return;
+    }
 
-//     const [res_user] = await db.query(
-//       `
-//             SELECT user_Code,user_Name,role_Id,user_Status FROM users 
-//             WHERE user_Code = ?
-//             AND user_Name = ? 
-//             AND role_Id = ?
-//             AND user_Status = ?`,
-//       [
-//         json["user_Code"],
-//         json["user_Name"],
-//         json["role_Id"],
-//         json["user_Status"],
-//       ]
-//     );
+    const [res_user] = await db.query(`
+      SELECT 
+      user_id
+      ,name AS user_name
+      ,role AS user_role
+      ,phone AS user_phone
+      FROM user 
+      WHERE name = ?`,
+      [json["user_name"]]
+    );
 
-//     if (res_user && res_user.length > 0) {
-//       res.send({
-//         data: res_user[0],
-//         status: "200",
-//         message: "SUCCESS",
-//         detail: "Authen successful",
-//       });
-//       return;
-//     }
+    if (res_user && res_user.length > 0) {
+      res.send({
+        data: res_user[0],
+        status: "200",
+        message: "SUCCESS",
+        detail: "Authen successful",
+      });
+      return;
+    }
 
-//     res.send({
-//       status: "404",
-//       message: "WARNING",
-//       detail: "No Data",
-//     });
-//   } catch (err) {
-//     res.send({ status: "500", message: "ERROR", detail: err.message });
-//   }
-// });
+    res.send({
+      status: "404",
+      message: "WARNING",
+      detail: "No Data",
+    });
+  } catch (err) {
+    res.send({ status: "500", message: "ERROR", detail: err.message });
+  }
+});
 
 //get
-// router.post("/get", async (req, res) => {
-//   try {
-//     const json = req.body;
+router.post("/get", async (req, res) => {
+  try {
+    const json = req.body;
 
-//     if (!json) {
-//       res.send({
-//         status: "400",
-//         message: "WARNING",
-//         detail: "No Data found",
-//       });
-//       return;
-//     }
+    if (!json) {
+      res.send({
+        status: "400",
+        message: "WARNING",
+        detail: "No Data found",
+      });
+      return;
+    }
 
-//     let page = parseInt(json["page"]) || 1;
-//     let pageSize = parseInt(json["pageSize"]) || 10;
-//     let offset = (page - 1) * pageSize;
+    let page = parseInt(json["page"]) || 1;
+    let pageSize = parseInt(json["pageSize"]) || 10;
+    let offset = (page - 1) * pageSize;
 
-//     let role_Id = "";
+    let user_role = "";
 
-//     if (json["role_Id"]) {
-//       role_Id = `A.role_Id = '${json["role_Id"]}' AND`;
-//     }
+    if (json["user_role"]) {
+      user_role = `role = '${json["user_role"]}' AND`;
+    }
 
-//     const query_search = `${role_Id}`;
+    const query_search = `${user_role}`;
 
-//     const [res_total] = await db.query(`SELECT COUNT(*) AS total FROM users`);
-//     const [res_user] = await db.query(
-//       `
-//         SELECT A.*,B.role_NameThai FROM users AS A 
-//         LEFT JOIN roles AS B ON B.role_Id = A.role_Id 
-//         WHERE ${query_search} A.role_Id != 1 AND CONCAT_WS(' ', A.user_Code,A.user_Name) LIKE '%' ? '%' LIMIT ? OFFSET ?`,
-//       [json["search"], pageSize, offset]
-//     );
+    const [res_total] = await db.query(`SELECT COUNT(*) AS total FROM user`);
+    const [res_user] = await db.query(`
+        SELECT * FROM user WHERE ${query_search} CONCAT_WS(' ',name) LIKE '%' ? '%' LIMIT ? OFFSET ?`,
+      [json["search"], pageSize, offset]
+    );
 
-//     if (res_user) {
-//       let total = parseInt(res_total[0].total);
-//       let totalPages = Math.ceil(total / pageSize);
-//       res.send({
-//         data: res_user,
-//         totalCount: total,
-//         totalPages: totalPages,
-//         status: "200",
-//         message: "SUCCESS",
-//         detail: "successful",
-//       });
-//       return;
-//     }
+    if (res_user) {
+      let total = parseInt(res_total[0].total);
+      let totalPages = Math.ceil(total / pageSize);
+      res.send({
+        data: res_user,
+        totalCount: total,
+        totalPages: totalPages,
+        status: "200",
+        message: "SUCCESS",
+        detail: "successful",
+      });
+      return;
+    }
 
-//     res.send({
-//       status: "404",
-//       message: "WARNING",
-//       detail: "No Data",
-//     });
-//   } catch (err) {
-//     res.send({ status: "500", message: "ERROR", detail: err.message });
-//   }
-// });
+    res.send({
+      status: "404",
+      message: "WARNING",
+      detail: "No Data",
+    });
+
+  } catch (err) {
+    res.send({ status: "500", message: "ERROR", detail: err.message });
+  }
+});
 
 // //getbyid
 router.post('/getbyid', async (req, res) => {
@@ -194,148 +190,126 @@ try {
   }
 });
 
-// //insert
-// router.post("/insert", async (req, res) => {
-//   try {
-//     const json = req.body;
+//insert
+router.post("/insert", async (req, res) => {
+  try {
+    const json = req.body;
 
-//     if (!json) {
-//       res.send({
-//         status: "400",
-//         message: "WARNING",
-//         detail: "No Data found",
-//       });
-//       return;
-//     }
+    if (!json) {
+      res.send({
+        status: "400",
+        message: "WARNING",
+        detail: "No Data found",
+      });
+      return;
+    }
 
-//     const userId = uuidv4();
-//     const [res_user] = await db.query(
-//       `SELECT * FROM users WHERE user_Name = ? AND user_IsUse = 1`,
-//       [json["user_Name"]]
-//     );
+    const [res_user] = await db.query(`SELECT * FROM user WHERE name = ? OR email = ?`,[json["user_name"],json["user_email"]]);
 
-//     if (res_user && res_user[0]) {
-//       res.send({
-//         status: "404",
-//         message: "WARNING",
-//         detail: "Duplicate Data",
-//       });
-//       return;
-//     }
+    if (res_user && res_user[0]) {
+      res.send({
+        status: "404",
+        message: "WARNING",
+        detail: "Duplicate Data",
+      });
+      return;
+    }
 
-//     await db.query(
-//       `INSERT INTO users (user_Id
-//             ,user_Code
-//             ,user_Name
-//             ,user_Password
-//             ,user_Status
-//             ,user_IsUse
-//             ,role_Id) VALUES (?,LPAD(CRC32(?), 13, '0'),?,?,?,?,?)`,
-//       [
-//         userId,
-//         userId,
-//         json["user_Name"],
-//         md5(json["user_Password"]).toUpperCase(),
-//         "1",
-//         "1",
-//         json["role_Id"],
-//       ]
-//     );
+    await db.query(`INSERT INTO user (name,password,email,phone) VALUES (?,?,?,?)`
+      ,[json["user_name"],md5(json["user_pass"]),json["user_email"],json["user_phone"]]);
 
-//     res.send({
-//       status: "200",
-//       message: "SUCCESS",
-//       detail: "Insert successful",
-//     });
-//   } catch (err) {
-//     res.send({ status: "500", message: "ERROR", detail: err.message });
-//   }
-// });
+    res.send({
+      status: "200",
+      message: "SUCCESS",
+      detail: "Insert successful",
+    });
+  } catch (err) {
+    res.send({ status: "500", message: "ERROR", detail: err.message });
+  }
+});
 
-// //update
-// router.post("/update", async (req, res) => {
-//   try {
-//     const json = req.body;
+//update
+router.post("/update", async (req, res) => {
+  try {
+    const json = req.body;
 
-//     if (!json) {
-//       res.send({
-//         status: "400",
-//         message: "WARNING",
-//         detail: "No Data found",
-//       });
-//       return;
-//     }
+    if (!json) {
+      res.send({
+        status: "400",
+        message: "WARNING",
+        detail: "No Data found",
+      });
+      return;
+    }
 
-//     const [res_user] = await db.query(`SELECT * FROM users WHERE user_Id = ?`, [
-//       json["user_Id"],
-//     ]);
+    const [res_user] = await db.query(`SELECT * FROM user WHERE user_id = ?`, [json["user_id"]]);
 
-//     if (res_user && res_user.length > 0) {
-//       let pass = "";
+    if (res_user && res_user.length > 0) {
+      let pass = "";
 
-//       if (json["user_new_password"]) {
-//         let userNewPassword = md5(json["user_new_password"]).toUpperCase();
-//         if (json["user_Password"] !== userNewPassword) {
-//           pass = userNewPassword;
-//         }
-//       } else {
-//         pass = json["user_Password"];
-//       }
+      if (json["user_new_pass"]) {
+        let userNewPass = md5(json["user_new_pass"]).toUpperCase();
+        if (json["user_pass"] !== userNewPass) {
+          pass = userNewPass;
+        }
+      } else {
+        pass = res_user[0].password
+      }
 
-//       await db.query(
-//         `
-//         UPDATE users SET user_Name = ?
-//         ,user_Password = ?
-//         ,user_Status = ?
-//         ,user_IsUse = ? WHERE user_Id`,
-//         [json["user_Name"], pass, json["user_Status"], json["user_IsUse"]]
-//       );
+      await db.query(
+        `
+        UPDATE user SET name = ?
+        ,password = ?
+        ,email = ?
+        ,phone = ? WHERE user_id = ?`,
+        [json["user_name"], pass, json["user_email"],json["user_phone"],json["user_id"]]
+      );
 
-//       res.send({
-//         status: "200",
-//         message: "SUCCESS",
-//         detail: "successful",
-//       });
+      res.send({
+        status: "200",
+        message: "SUCCESS",
+        detail: "successful",
+      });
 
-//       return;
-//     }
+      return;
+    }
 
-//     res.send({
-//       status: "404",
-//       message: "WARNING",
-//       detail: "No Data",
-//     });
-//   } catch (err) {
-//     res.send({ status: "500", message: "ERROR", detail: err.message });
-//   }
-// });
+    res.send({
+      status: "404",
+      message: "WARNING",
+      detail: "No Data",
+    });
+  } catch (err) {
+    res.send({ status: "500", message: "ERROR", detail: err.message });
+  }
+});
 
-// //delete
-// router.post('/delete', async (req, res) => {
-//   try {
-//       const json = req.body;
+//delete
+router.post('/delete', async (req, res) => {
+  try {
+      const json = req.body;
 
-//       if (!json['user_Id']) {
-//         res.send({
-//           status: "400",
-//           message: "WARNING",
-//           detail: "No Data found",
-//         });
-//         return;
-//       }
+      if (!json['user_id']) {
+        res.send({
+          status: "400",
+          message: "WARNING",
+          detail: "No Data found",
+        });
+        return;
+      }
 
-//         await db.query(`DELETE FROM users WHERE user_Id = ?`,[json["user_Id"]]);
+        await db.query(`DELETE FROM user WHERE user_id = ?`,[json["user_id"]]);
 
-//         res.send({
-//           status: "200",
-//           message: "SUCCESS",
-//           detail: "successful",
-//         });
-//         return
+        res.send({
+          status: "200",
+          message: "SUCCESS",
+          detail: "successful",
+        });
+        return
 
-//     } catch (err) {
-//       res.send({ status: "500", message: 'ERROR',detail:err.message });
-//     }
-// });
+    } catch (err) {
+      res.send({ status: "500", message: 'ERROR',detail:err.message });
+    }
+});
 
 module.exports = router;
